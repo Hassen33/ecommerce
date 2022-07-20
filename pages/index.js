63e48daf95c3189c9a5f5eb2173c7cde5ed05ad7
hassen <hassenbj33@gmail.com>
2022-07-20 20:13:04 +0100

    contextLogic

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Product, FooterBanner, HeroBanner } from '../components';
 import { client } from '../lib/client'; 
-
 const Home = ({ products, bannerData }) => {
   return (
-    <div>
+    <>
       {/* hero banner */}
           <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+      {console.log(bannerData)};
     <div className="products-heading">
       <h2>Best Seller Products</h2>
       <p>speaker There are many variations passages</p>
+      </div>
+      
+      
+      <div className="products-container">
+      {products?.map((product) => <Product key={product._id} product={product} />)}
     </div>
-    </div>
+
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  
+    </>
+ 
 
 
   )
